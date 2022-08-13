dad12d9461aabebbcfe029d17305cd19c681a9c6
ranjan1122 <50030617+ranjan1122@users.noreply.github.com>
2022-08-14 00:25:12 +0530

    Update App.java
    Ok I am adding one "-" in between.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!"+"Please add my changes.." );
+        System.out.println( "Hello World!-"+"Please add my changes.." );
     }
 }
